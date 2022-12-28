d9d9a01b2cbf410276baa2675d225a69e59e4ecf
mikaelmach <michaelmach3000@gmail.com>
2022-12-28 13:57:45 +0100

    feat: added install script dummy, and dummy common files

---
+++ common/install_i3.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+set -Eeuo pipefail
+
+
+#######################################
+# install configs
+# Globals:
+# Arguments:
+#   None
+#######################################
+function install_() {
+	
+  }
---
+++ install_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+source common/install_ohmyzsh.sh
+source common/install_powerfonts.sh
+source common/install_configs.sh
+source common/install_i3.sh
+source common/install_terminator.sh
+source common/install_fzf.sh
